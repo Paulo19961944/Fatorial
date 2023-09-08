e077b1722fca00904a8b01827a202f1cde34fa39
Paulo Henrique <paulo24041996@gmail.com>
2023-09-08 16:43:17 -0700

    Fatorial

---
+++ src/com/github/www/Fatorial.java
@@ -0,0 +1,41 @@
+package com.github.www;
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        // Chama a Classe Scanner
+        Scanner teclado = new Scanner(System.in);
+
+        // Captura o número
+        System.out.println();
+        System.out.println("********** RESULTADO **********");
+        System.out.println();
+        System.out.print("Digite um número (entre 1 e 100): ");
+        long numero = teclado.nextLong();
+
+        // Verifica se o número está dentro do intervalo permitido
+        if (numero >= 1 && numero <= 100) {
+            // Calcula o Fatorial
+            BigInteger fatorial = calcularFatorial(BigInteger.valueOf(numero));
+
+            // Printa o Resultado
+            System.out.println();
+            System.out.println("********** RESULTADO **********");
+            System.out.println();
+            System.out.println("O Resultado é: " + fatorial);
+            System.out.println();
+            System.out.println("********** PAULO HENRIQUE AZEVEDO DO NASCIMENTO");
+        } else {
+            System.out.println("Número fora do intervalo permitido (1 a 100).");
+        }
+    }
+
+    public static BigInteger calcularFatorial(BigInteger n) {
+        BigInteger resultado = BigInteger.ONE;
+        for (BigInteger i = BigInteger.valueOf(2); i.compareTo(n) <= 0; i = i.add(BigInteger.ONE)) {
+            resultado = resultado.multiply(i);
+        }
+        return resultado;
+    }
+}
